3e4cbee4b264d052a93ad2e12f16798cda733dd3
symbianl <zxgzhanghu@gmail.com>
2017-10-27 11:42:22 +0800

    邮箱地址不可重复注册（邮箱唯一性）

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+#  -*- coding:utf-8 -*-
 from django.contrib.auth.forms import UserCreationForm
 from blog.models import User
 
@@ -5,3 +6,12 @@ class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
        model = User
        fields = ('username','first_name','last_name','email',)
+
+    def clean(self):
+        cleaned_data = super(RegisterForm,self).clean()
+        email =cleaned_data.get('email')
+        db_email = User.objects.filter(email=email)
+        if email in [email.email for email in db_email]:
+            self._errors['email'] = self.error_class([u'邮箱以存在，请换一个'])
+
+            return cleaned_data
